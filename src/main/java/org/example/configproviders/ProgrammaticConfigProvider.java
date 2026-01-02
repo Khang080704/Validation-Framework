f186a3e69807bab3aead534cc645068e64d96583
Nguyen Thi Huyen <22120143@student.hcmus.edu.vn>
2026-01-02 22:43:34 +0700

    Implement ProgrammaticConfigProvider and ConstraintBuilder

---
+++ src/main/java/org/example/configproviders/ProgrammaticConfigProvider.java
@@ -0,0 +1,20 @@
+package org.example.configproviders;
+
+import org.example.common.FieldConfig;
+
+import java.util.Map;
+import java.util.Set;
+
+public class ProgrammaticConfigProvider implements ConfigProvider {
+    private Map<Class<?>, Set<FieldConfig>> configs;
+
+    @Override
+    public Set<FieldConfig> getConfig(Class<?> type) {
+        Set<FieldConfig> fieldConfigs = configs.get(type);
+        return fieldConfigs != null ? fieldConfigs : Set.of();
+    }
+
+    public void putConfigs(Class<?> type, Set<FieldConfig> fieldConfigs) {
+        configs.put(type, fieldConfigs);
+    }
+}
